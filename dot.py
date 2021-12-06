0ca35ad6865ae82615aab44b878a5ddfbcc7cf5b
Angelo-Bruno <brunoangelo.dev@gmail.com>
2021-12-06 10:21:19 -0100

    changed some function, upgraded code performance

--- dot.py
+++ dot.py
@@ -14,7 +14,7 @@ UP = 3
 LEFT = 7
 RIGHT = 9
 movements = {0: UP, 1: DOWN, 2: LEFT, 3: RIGHT}
-MOVESPEED = 2
+
 FPS = 60
 
 windowsSurface = pg.display.set_mode((W_WIDTH, W_HEIGHT), 0, 32)
@@ -24,20 +24,23 @@ fps_Clock = pg.time.Clock()
 
 class Dot(pg.Rect):
 
-    def __init__(self) -> None:
+    def __init__(self,color=False,radius=False) -> None:
         self.x = rnd.randint(0, W_WIDTH)
         self.y = rnd.randint(0, W_HEIGHT)
-        self.color = COLORS['RED']
-        self.radius = 3
+        self.color = color if color else COLORS['RED']
+        self.radius = radius if radius else 3
         self.speedx = rnd.randint(1, 3)
         self.speedy = rnd.randint(1, 3)
         self.dot = pg.Rect(self.x, self.y, 8, 8)
         # self.vision = 80
-        self.connected = False
+        # self.connected = False
 
     def draw(self) -> None:
         pg.draw.circle(windowsSurface, self.color,
                        (self.x, self.y), self.radius)
+        
+    def dist_between(self,dot2:pg.Rect):
+        return abs(self.x-dot2.x) + abs(dot2.y-self.y)
 
     def move(self):
         if self.y < 0:            # block has moved past the top
@@ -50,62 +53,53 @@ class Dot(pg.Rect):
             self.x -= W_WIDTH
         self.draw()
 
-
-def dist_between(x, y): return (abs(x.x-y.x)+abs(y.y-x.y))
-
-
-dotList: list = []
-partList = []  # list of particles for the background[stars effect]
-visited = []
-
-for i in range(0, 220):
-    part = Dot()
-    part.color = COLORS['WHITE']
-    part.radius = 1
-    partList.append(part)
-    dot: Dot = Dot()
-    dotList.append(dot)
+    def change_direction(self,movespeed,can_move=False):  
+            movespeed*=-1
+            if can_move and rnd.random() < 0.03:
+                self.speedy = rnd.randint(0,3)*movespeed
+            elif rnd.random() < 0.03:
+                self.speedx = rnd.randint(0,3)*movespeed
+            self.left+=self.speedx
+            self.top+=self.speedy
 
 
-while True:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-            exit()
 
-    windowsSurface.fill(COLORS['BLACK'])
-    can_Move = True
-    for i, dot_i in enumerate(dotList):
-        dot_2: Dot
-        if can_Move:
-            movey = rnd.random()
-            if movey <= 0.03:
-                MOVESPEED *= -1
-                dot_i.speedy = rnd.randint(0, 3)*MOVESPEED
+                
+def draw_line(screen,dots:list, mouse)->None:
+    mouse_pos = pg.mouse.get_pos() if mouse else (-100,-100,-100)
+    mouse_rect = pg.Rect(mouse_pos[0],mouse_pos[1],6,6) if mouse else 0
+    for i, dot_i in enumerate(dots):
+        for dot_j in dots[i+1:]:
+            if dot_i.dist_between(dot_j) < 80:
+                pg.draw.line(screen,COLORS['BLUE'],(dot_i.x, dot_i.y), (dot_j.x, dot_j.y),2)
+        if mouse and dot_i.dist_between(mouse_rect) < 200:
+            pg.draw.line(screen,COLORS['GREEN'],(mouse_rect.x,mouse_rect.y),(dot_i.x,dot_i.y),2)
+        
 
-        else:
-            movex = rnd.random()
-            if movex <= 0.03:
-                MOVESPEED *= -1
-                dot_i.speedx = rnd.randint(0, 3)*MOVESPEED
+dotList = [Dot() for _ in range(250)]
+partList = [Dot(COLORS['WHITE'],1) for _ in range(250)]  # list of particles for the background[stars effect]
 
-        dot_i.left += dot_i.speedx
-        dot_i.top += dot_i.speedy
-        can_Move = False
-        if event.type == pg.MOUSEMOTION:
-            mouse_pos = pg.mouse.get_pos()
-            mouse = pg.Rect(mouse_pos[0], mouse_pos[1], 6, 6)
-            if dist_between(mouse, dot_i) < 200:
-                pg.draw.line(windowsSurface, COLORS['GREEN'],
-                             (mouse.x, mouse.y), (dot_i.x, dot_i.y), 2)
-        for j, dot_j in enumerate(dotList):
-            visited.append(dot_j)
-            if dist_between(dot_i, dot_j) < 80 and dot_i not in visited:
-                pg.draw.line(windowsSurface, COLORS['BLUE'],
-                             (dot_i.x, dot_i.y), (dot_j.x, dot_j.y), 2)
-        visited.clear()    
-        dot_i.move()
-        partList[i].draw()
-    pg.display.flip()
-    fps_Clock.tick(FPS)
-    time.sleep(0.02)
+
+def main():
+    MOVESPEED = 2
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                exit()
+        windowsSurface.fill(COLORS['BLACK'])
+        can_Move = True
+        mouse = event.type == pg.MOUSEMOTION
+        draw_line(windowsSurface,dotList,mouse)
+        
+        for i, dot_i in enumerate(dotList):
+            dot_i.change_direction(MOVESPEED,can_Move)
+            can_Move = False
+            dot_i.move()
+            partList[i].draw()
+            
+        pg.display.flip()
+        fps_Clock.tick(FPS)
+        time.sleep(0.02)
+
+main()
